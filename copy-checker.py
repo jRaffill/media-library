eef96bb7f1496b86cb2353b378048967ab5ce116
Jayalakshmi Raffill <jraffill@Jayalakshmis-MacBook-Pro.local>
2023-01-09 15:04:01 -0800

    Add basic checking functionality
    Tests done. Output:
    ```Original file test-dir/a/200-300.jpg
    Original file test-dir/a/002-003.jpg
    Original file test-dir/b/200-500.jpg
    Original file test-dir/b/300-100.jpg
    Original file test-dir/b/c/200-400.jpg
    Original file test-dir/b/c/002-004.jpg
    ['200-300.jpg', '002-003.jpg', '200-500.jpg', '300-100.jpg', '200-400.jpg', '002-004.jpg']
    ['test-dir/a', 'test-dir/a', 'test-dir/b', 'test-dir/b', 'test-dir/b/c', 'test-dir/b/c']
    Copied file test-dir-copy/a/COPY_200-300.jpg
    Copied file test-dir-copy/b/200-500.jpg
    Copied file test-dir-copy/b/300-100.jpg
    Copied file test-dir-copy/b/c/abc_002-004.jpg
    ['COPY_200-300.jpg', '200-500.jpg', '300-100.jpg', 'abc_002-004.jpg']
    ['test-dir-copy/a', 'test-dir-copy/b', 'test-dir-copy/b', 'test-dir-copy/b/c']
    ------------------------
    Found a possible match between original file 200-300.jpg and new file COPY_200-300.jpg
    ****Found missing file test-dir/a/002-003.jpg****
    Found a match between original file 200-500.jpg and new file 200-500.jpg
    Found a match between original file 300-100.jpg and new file 300-100.jpg
    ****Found missing file test-dir/b/c/200-400.jpg****
    Found a possible match between original file 002-004.jpg and new file abc_002-004.jpg```
    
    Next step: implement hash checking.

--- copy-checker.py
+++ copy-checker.py
@@ -3,12 +3,21 @@ import os
 oldpath = "test-dir/"
 newpath = "test-dir-copy/"
 
+oldfiles = []
+oldroots = []
+newfiles = []
+newroots = []
+
 for i in os.walk(oldpath):
   root = i[0]
   files = i[2]
   for file in files:
     if (file != ".DS_Store"):
       print("Original file " + root + "/" + file)
+      oldfiles += [file]
+      oldroots += [root]
+print(oldfiles)
+print(oldroots)
 
 for i in os.walk(newpath):
   root = i[0]
@@ -16,3 +25,27 @@ for i in os.walk(newpath):
   for file in files:
     if (file != ".DS_Store"):
       print("Copied file " + root + "/" + file)
+      newfiles += [file]
+      newroots += [root]
+print(newfiles)
+print(newroots)
+
+print("------------------------")
+
+newindex = 0
+missingfiles = []
+missingroots = []
+for file in oldfiles:
+  root = oldroots[oldfiles.index(file)]
+  newfile = newfiles[newindex]
+  if (file == newfile):
+    print("Found a match between original file " + file + " and new file " + newfile)
+    newindex += 1
+  elif (file in newfile):
+    print("Found a possible match between original file " + file + " and new file " + newfile)
+    newindex += 1
+  else:
+    print("****Found missing file " + root + "/" + file + "****")
+    missingfiles += [file]
+    missingroots += [root]
+
