410ec7437c213e7e4981fde913bd048607119ba8
jRaffill <jayalakshmi@damajash.org>
2023-11-23 20:30:33 -0800

    Address review by @shankari
    I added a few comments to explain what my code is doing, and also reworked most of the logic to just check every file against every other file.
    Output:
    
    Original file test-dir/a/200-300.jpg
    Original file test-dir/a/002-003.jpg
    Original file test-dir/b/200-500.jpg
    Original file test-dir/b/300-100.jpg
    Original file test-dir/b/c/200-400.jpg
    Original file test-dir/b/c/002-004.jpg
    Copied file test-dir-copy/a/200-300_COPY.jpg
    Copied file test-dir-copy/b/200-500.jpg
    Copied file test-dir-copy/b/300-100.jpg
    Copied file test-dir-copy/b/c/abc_002-004.jpg
    ------------------------
    Found a match between original file 200-300.jpg and new file 200-300_COPY.jpg
    ****Found missing file test-dir/a/002-003.jpg****
    Found a match between original file 200-500.jpg and new file 200-500.jpg
    Found a match between original file 300-100.jpg and new file 300-100.jpg
    ****Found missing file test-dir/b/c/200-400.jpg****
    Found a match between original file 002-004.jpg and new file abc_002-004.jpg
    All missing files:
    ['test-dir/a/002-003.jpg', 'test-dir/b/c/200-400.jpg']

--- copy-checker.py
+++ copy-checker.py
@@ -9,6 +9,9 @@ oldroots = []
 newfiles = []
 newroots = []
 
+# Go through the old directory and save file locations and roots to corresponding directories
+# (They are linked by their shared index position which is bad or whatever but I'm too lazy
+# to make an object just for this)
 for i in os.walk(oldpath):
   root = i[0]
   files = i[2]
@@ -18,6 +21,7 @@ for i in os.walk(oldpath):
       oldfiles += [file]
       oldroots += [root]
 
+# Go through the new directory and save file locations and roots
 for i in os.walk(newpath):
   root = i[0]
   files = i[2]
@@ -29,26 +33,22 @@ for i in os.walk(newpath):
 
 print("------------------------")
 
-newindex = 0
 missingfiles = []
 
+# Loop through both old files and new files to find matches
 for file in oldfiles:
-  root = oldroots[oldfiles.index(file)]
-  newfile = newfiles[newindex]
-  filepath = os.path.join(root, file)
-  if (file == newfile):
-    print("Found a match between original file " + file + " and new file " + newfile)
-    newindex += 1
-  elif (file in newfile):
-    newfilepath = os.path.join(newroots[newindex], newfile)
+  foundmatch = False
+  for newfile in newfiles:
+    root = oldroots[oldfiles.index(file)]
+    newroot = newroots[newfiles.index(newfile)]
+    filepath = os.path.join(root, file)
+    newfilepath = os.path.join(newroot, newfile)
     oldfilehash = hashlib.md5(open(filepath, "rb").read()).hexdigest()
     newfilehash = hashlib.md5(open(newfilepath, "rb").read()).hexdigest()
-    if (oldfilehash == newfilehash):  
+    if (oldfilehash == newfilehash):
+      foundmatch = True
       print("Found a match between original file " + file + " and new file " + newfile)
-      newindex += 1
-    else:
-      print("****Found missing file " + filepath + "****")
-  else:
+  if (not foundmatch):
     print("****Found missing file " + filepath + "****")
     missingfiles += [filepath]
 
